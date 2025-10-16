d44bf0e2ec6a8dff30ecc8b25d090aed6cde33d5
sinisaglavas <sinisaglavas@ymail.com>
2025-10-16 17:22:34 +0200

    Practice 27.18 - change column 'status' to default 'unassigned'

---
+++ database/migrations/2025_10_16_151754_set_default_status_to_shipments.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            $table->string('status', 12)->default(\App\Models\Shipment::STATUS_UNASSIGNED)
+            ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            $table->dropColumn('status'); // smisao da ako radimo rollback da obrise ovo polje
+        });
+    }
+};
